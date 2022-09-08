1f8a82dc5a177ae04a63a042386e7a0a0d7778e2
Diana-ak <dianaambani15@gmail.com>
2022-09-08 16:02:29 -0700

    commit 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
 
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Main -Entry point
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 
