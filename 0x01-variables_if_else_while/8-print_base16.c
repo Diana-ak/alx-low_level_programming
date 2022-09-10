03c2f1b0fbd01d8806fe918b3d3fffbcc03e2a34
Diana-ak <dianaambani15@gmail.com>
2022-09-09 18:26:07 -0700

    nineth commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int k;
+	char alphabet;
+
+	for (k = '0'; k < 10; k++)
+	{
+		putchar(k);
+	}
+	k = '1';
+	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
