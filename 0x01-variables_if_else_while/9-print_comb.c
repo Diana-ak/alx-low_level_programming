986ec7024f3002c83e232f4b411f9b1128e529b5
Diana-ak <dianaambani15@gmail.com>
2022-09-09 19:03:10 -0700

    tenth commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 {
 	int k;
 
-	for (k = '0'; k < '19'; k++)
+	for (k = '0'; k <= '9'; k++)
 	{
 		putchar(k);
-		if (k != 19)
+		if (k != 9)
 		{
 			putchar(',');
 			putchar(' ');
