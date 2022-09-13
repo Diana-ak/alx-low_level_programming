db5d1cd70b59c745ba6978a600ca5b4561502744
Diana-ak <dianaambani15@gmail.com>
2022-09-13 14:23:37 -0700

    commit 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - lowercase, followed by new line
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
