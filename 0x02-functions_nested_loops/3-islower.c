b5538aa17c04a0de0aee54ab35b36ccf55499aa4
Diana-ak <dianaambani15@gmail.com>
2022-09-13 15:01:23 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower -checks lowercase
+ *
+ * Return: 1 if lowercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+
+	{
+		return (0);
+	}
+}
