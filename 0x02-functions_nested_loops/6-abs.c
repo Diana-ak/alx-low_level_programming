a236a6319d6dbb6638b50e0e60083ec2643b5eb4
Diana-ak <dianaambani15@gmail.com>
2022-09-13 16:51:49 -0700

    commit 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include <stdio.d>
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of integer
+ *@int - int is an integer
+ *
+ * Return: Absolute value of integer
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
