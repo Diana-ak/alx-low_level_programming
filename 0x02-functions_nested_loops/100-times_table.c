e53f0adccb7dd4e2dd6b541c2564c8b083108c83
Diana-ak <dianaambani15@gmail.com>
2022-09-14 08:12:53 -0700

    commit 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,34 +11,33 @@ void print_times_table(int n)
 {
 	int r, c, p;
 
-	if (n > 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (r = 0; r <= n; r++)
 		{
-			_putchar(40);
+			_putchar(48);
 			for (c = 0; c <= n; c++)
 			{
 				p = r * c;
-				_putchar(40);
-				_putchar(30);
+				_putchar(44);
+				_putchar(32);
 				if (p < 10)
 				{
-					_putchar(30);
-					_putchar(30);
-					_putchar(p + 40);
+					_putchar(32);
+					_putchar(32);
+					_putchar(p + 48);
 				}
-				else if (p >= 10 && p <= 100)
+				else if (p < 100)
 				{
-					_putchar(40);
-					_putchar(40);
-					_putchar((p / 10) + 40);
-					_putchar((p % 10) + 40);
+					_putchar(32);
+					_putchar((p / 10) + 48);
+					_putchar((p % 10) + 48);
 				}
 				else
 				{
-					_putchar(((p / 100) % 10) + 40);
-					_putchar(((p / 10) % 10) + 40);
-					_putchar((p % 10) + 40);
+					_putchar(((p / 100) % 10) + 48);
+					_putchar(((p / 10) % 10) + 48);
+					_putchar((p % 10) + 48);
 				}
 			}
 			_putchar('\n');
