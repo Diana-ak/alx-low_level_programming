26c3e37ce2109cddb5c23967be6fd11b02e63782
Diana-ak <dianaambani15@gmail.com>
2022-09-14 10:43:37 -0700

    commit 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,13 +12,13 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{ 
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		while (n > 98)
 		{
@@ -26,5 +26,5 @@ void print_to_98(int n)
 			n--;
 		}
 	}
-	printf("\n");
+	printf("98\n");
 }
