584b3fdfb2530875a68d991ea3a90f83a8720ecc
Diana-ak <dianaambani15@gmail.com>
2022-09-14 04:48:22 -0700

    commit 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,5 +9,39 @@
 
 void print_times_table(int n)
 {
-	return (0);
+	int r, c, p;
+
+	if (n >= 0 && n < 15)
+	{
+		for (r = 0; r <= n, r++)
+		{
+			for (c = 0; c <= n, c++)
+			{
+				p = r * c;
+				if (c = 0)
+					_putchar('0');
+				else if (p < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((p % 10) + '0');
+				}
+				else if (p >=10 && p<100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((p / 10) % 10) + '0');
+					_putchar((p % 10) + '0');
+				}
+				else if (p >= 100 && p < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(((p / 100) % 100) + '0');
+					_putchar((p % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
