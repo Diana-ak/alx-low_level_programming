d66c49a96cf22735c5d1712cc25317885d7644f8
Diana-ak <dianaambani15@gmail.com>
2022-09-13 17:19:48 -0700

    commit 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *@n: n is an integer
+ *
+ * Return: last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	if (n > 0; n < 10; n++)
+		last == 9
+			_putchar((last) + '0');
+
+	return (last);
+}
