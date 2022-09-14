69c3f5e0b03d13fa30f70bda47d28d547011b93c
Diana-ak <dianaambani15@gmail.com>
2022-09-13 18:44:52 -0700

    commit 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table
+ *
+ * Return: times table
+ */
+
+void times_table(void)
+{
+	int row, colum;
+
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (colum = 0; colum <= 9; colum++)
+		{
+			product = (row * colum);
+			if ((product / 10) > 0)
+			{
+				_putchar((product / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((product % 10) + '0');
+
+			if (colum < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+
+	}
+}
