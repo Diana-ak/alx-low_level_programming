97bfd097671fa34176445fc94daadd10033cdb72
Diana-ak <dianaambani15@gmail.com>
2022-09-14 04:52:50 -0700

    commit 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,12 +13,12 @@ void print_times_table(int n)
 
 	if (n >= 0 && n < 15)
 	{
-		for (r = 0; r <= n, r++)
+		for (r = 0; r <= n; r++)
 		{
-			for (c = 0; c <= n, c++)
+			for (c = 0; c <= n; c++)
 			{
 				p = r * c;
-				if (c = 0)
+				if (c == 0)
 					_putchar('0');
 				else if (p < 10)
 				{
