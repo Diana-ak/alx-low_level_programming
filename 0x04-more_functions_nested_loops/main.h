ea01d341a2041c9f00b2f28bcc1b1c968f4d7400
Diana-ak <dianaambani15@gmail.com>
2022-09-15 11:09:36 -0700

    commit 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,64 +1,23 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * main - header file for prototype
- * void prototype(void)
- * int prototype (int)
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
  */
 
-/**
- * _isupper - determines uppercase or not
- * @c: character to determine
- * Return: true or false
- */
-int _isupper(int c);
-/**
- * _isdigit - checks if its digit
- * @c: character to check
- *
- * Return: returns boolean
- */
-int _isdigit(int c);
-/**
- * mul -multiplies stuff
- * @a: first integer
- * @b: second integer
- * Return: gives result
- */
-int mul(int a, int b);
-/**
- * print_numbers - prints the numbers
- *
- */
 void print_numbers(void);
-/**
- * print_most_numbers - prints the numbers
- *
- */
 void print_most_numbers(void);
-/**
- * more_numbers - prints numbers
- */
 void more_numbers(void);
-/**
- * print_line - prints a line
- *
- * @n: integer to print stuff
- */
 void print_line(int n);
-/**
- * print_diagonal - prints diagonoal
- * @n: integer to know how may lines
- */
 void print_diagonal(int n);
-/**
- * print_square - prints a square
- * @size: prints the size of whatever 
- */
 void print_square(int size);
-/**
- * print_triangle - prints triangle
- * @size: size of triangle
- */
 void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
