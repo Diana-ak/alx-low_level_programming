f1f312344f33d54821d8b27c029d369f02b7a539
Diana-ak <dianaambani15@gmail.com>
2022-09-15 13:14:14 -0700

    commit 2

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,18 +3,16 @@
 
 /**
  * print_numbers - function that print all numbers from
- * 0 to 9
  *
- * Return: returns nothing
+ *
  */
-
 void print_numbers(void)
 {
-	int n;
+	int i;
 
-	for (n = 0; n <= 9; n++)
+	for (i = '0'; n <= '9'; i++)
 	{
-		_putchar(n);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
