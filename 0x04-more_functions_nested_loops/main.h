4a6661beeaa36c47e1863d420a1c7d5df4562f2b
Diana-ak <dianaambani15@gmail.com>
2022-09-15 09:55:41 -0700

    commit 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,13 +1,58 @@
-#ifndef _MAIN_
-#define _MAIN_
+#ifndef _MAIN_H
+#define _MAIN_H
 
+/**
+ * _isupper - determines uppercase or not
+ * @c: character to determine
+ * Return: true or false
+ */
 int _isupper(int c);
+/**
+ * _isdigit - checks if its digit
+ * @c: character to check
+ *
+ * Return: returns boolean
+ */
 int _isdigit(int c);
+/**
+ * mul -multiplies stuff
+ * @a: first integer
+ * @b: second integer
+ * Return: gives result
+ */
 int mul(int a, int b);
+/**
+ * print_numbers - prints the numbers
+ *
+ */
 void print_numbers(void);
+/**
+ * print_most_numbers - prints the numbers
+ *
+ */
 void print_most_numbers(void);
+/**
+ * more_numbers - prints numbers
+ */
 void more_numbers(void);
+/**
+ * print_line - prints a line
+ *
+ * @n: integer to print stuff
+ */
 void print_line(int n);
+/**
+ * print_diagonal - prints diagonoal
+ * @n: integer to know how may lines
+ */
 void print_diagonal(int n);
+/**
+ * print_square - prints a square
+ * @size: prints the size of whatever 
+ */
 void print_square(int size);
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ */
 void print_triangle(int size);
