c2183f0659c909f8cd64a4d9ee0aad2e30714762
Diana-ak <dianaambani15@gmail.com>
2022-09-19 13:43:36 -0700

    six

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other function of the string
+ * @str: parameter of the string
+ *
+ */
+
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		putchar(str[i]);
+	putchar('\n');
+}
