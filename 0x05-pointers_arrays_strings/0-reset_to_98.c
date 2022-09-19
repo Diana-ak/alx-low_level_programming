271a4836935bdfedd7409e3841afb04b4218e4d5
Diana-ak <dianaambani15@gmail.com>
2022-09-19 07:29:37 -0700

    zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes function to parameter and updates it
+ * @n: pointer to be updated
+ *
+ * Return: returns nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int x = 0;
+	int *n = &x;
+
+	*n = 98;
+}
