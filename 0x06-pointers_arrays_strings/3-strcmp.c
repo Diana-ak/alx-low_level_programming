ec63cac75be943584860b028645c6afe43fbecb0
Diana-ak <dianaambani15@gmail.com>
2022-09-22 13:25:24 -0700

    commit 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,8 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i, diff;
-	diff= 0;
+
+	diff = 0;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
@@ -24,4 +25,3 @@ int _strcmp(char *s1, char *s2)
 	}
 	return (diff);
 }
-
