b1f235afcca8fe2dbeaea1e930facab4015731b7
Diana-ak <dianaambani15@gmail.com>
2022-09-30 15:18:28 -0700

    commit 1

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,15 @@ int main(int argc, char *argv[])
 {
 	int i, j, product;
 
-	if (argc <= 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
-		{
-			i = atoi(argv[1]);
-			j = atoi(argv[2]);
-			product = i * j;
-		}
-		printf("%d\n", product);
 	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	product = i * j;
+	printf("%d\n", product);
 	return (0);
 }
