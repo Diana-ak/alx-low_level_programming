b71d7e8051371735d83c31bb30c8bbace3f7513f
Diana-ak <dianaambani15@gmail.com>
2022-09-30 16:21:36 -0700

    commit 2

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,22 +11,40 @@
 
 int main(int argc, char *argv[])
 {
-	int amount; /* cents to give back*/
-	int coins;
+	int amount, coins;
 
-	if (argc != 1)
+	if (argc != 2)
 	{
-		printf("Error\n", argc);
+		printf("Error\n");
 		return (1);
 	}
-	else if (coins < 0)
+	amount = atoi(argv[1]);
+	coins = 0;
+	if (amount > 25)
 	{
-		printf("0\n");
+		while (amount >= 25)
+			amount -= 25, coins++;
 	}
-	amount = atoi(argv[coins]);
-	while (amount == 1 || amount == 2 || amount == 5 ||
+	if (amount > 10 && amount < 25)
+	{
+		while (amount >= 10)
+			amount -= 10, coins++;
+	}
+	if (amount > 5 && amount < 10)
+	{
+		while (amount >= 5)
+			amount -= 5, coins++;
+	}
+	if (amount > 2 && amount < 5)
+	{
+		while (amount >= 2)
+			amount -= 2, coins++;
+	}
+	if (amount == 1 || amount == 2 || amount == 5 ||
 			amount == 10 || amount == 25)
+	{
 		coins++;
+	}
 	printf("%d\n", coins);
 	return (0);
 }
