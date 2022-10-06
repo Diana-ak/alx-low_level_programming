7f80046fa9414dd688a320a845a564735f6ae990
Diana-ak <dianaambani15@gmail.com>
2022-10-06 10:34:50 -0700

    commit 2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -113,10 +113,10 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 	for (; mult_len >= 0; mult_len--, mult--, prod--)
 	{
 		if (*mult < '0' || *mult > '9')
-			{
-				printf("Error\n");
-				exit(98);
-			}
+		{
+			printf("Error\n");
+			exit(98);
+		}
 		num = (*mult - '0') * digit;
 		num += tens;
 		*prod = (num % 10) + '0';
