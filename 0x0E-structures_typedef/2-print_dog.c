2182f3c8a78da3c0768e09ce468b5936954f0898
Diana-ak <dianaambani15@gmail.com>
2022-10-11 13:26:13 -0700

    commit 1

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,10 +14,14 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	else if (d->name)
+	else
 		printf("Name: %s\n", d->name);
-	if (d->age)
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
 		printf("Age: %f\n", d->age);
-	if (d->owner)
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
 		printf("Owner: %s\n", d->owner);
 }
