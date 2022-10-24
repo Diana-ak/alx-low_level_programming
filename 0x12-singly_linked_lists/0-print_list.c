cdd4b6b01e8f7126efe1a9e3663d0f0db375ef68
Diana-ak <dianaambani15@gmail.com>
2022-10-24 11:24:22 -0700

    commit 1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,9 +9,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t Num;
-
-	Num = 0;
+	size_t nelem;
+	
+	nelem = 0;
 
 	while (h != NULL)
 	{
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		Num++;
+		nelem++;
 	}
-	return (Num);
+	return (nelem);
 }
